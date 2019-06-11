22e0d14a46571a0a643efec284a896b614dc7466
Cheery Wang <cheeryone821@gmail.com>
2019-06-11 15:40:35 -0700

    Send multiple hw reminders

--- scripts/my_script.py
+++ scripts/my_script.py
@@ -7,17 +7,17 @@ sys.path.append('../')
 
 # Imports
 import datetime as dt
-from my_module.functions import send_email, send_email_at
+from my_module.functions import send_email, send_timed_email
 
 ###
 ###
-
+hws = [ ]
 t = dt.datetime.today()
 while True:
     hw = input("What homework do you have? (Enter Done if you don't have any homework to enter)")
     if hw == "Done":
         break
-    
+
     ddl = input("When is the deadline for this homework? (hr mm dd)")
     ddl_h = int(ddl[0:2])
     ddl_m = int(ddl[3:5])
@@ -33,19 +33,20 @@ while True:
         print("Please enter valid values for hour(00-23), month(01-12), day(01-31)")
     
     date = input("When do you want to be reminded of doing this homework? (hr mm dd)")
-    date_h = int(ddl[0:2])
-    date_m = int(ddl[3:5])
-    date_d = int(ddl[6:8])
+    date_h = int(date[0:2])
+    date_m = int(date[3:5])
+    date_d = int(date[6:8])
+    date_min = int(date[9:11]) # REMOVE LATER
     date_dt = t
     try:
         if date_m < t.month:
             t.year += 1 # handling reminder dates that are on the next year
-        date_dt = dt.datetime(t.year, date_m, date_d, date_h)
+        date_dt = dt.datetime(t.year, date_m, date_d, date_h, date_min)
     except TypeError:
         print("Please enter integers as hour, month, day")
     except ValueError:
         print("Please enter valid values for hour(00-23), month(01-12), day(01-31)")
-
+    hws.append(tuple((hw, ddl_dt, date_dt)))
 
 password = input("Type your password and press enter: ")
-send_email_at(password, hw, ddl_dt, date_dt)
+send_timed_email(password, hws)
