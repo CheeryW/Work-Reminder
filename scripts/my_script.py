d407de988759a70ba59f14c3ef8245b3ae396d04
Cheery Wang <cheeryone821@gmail.com>
2019-06-11 17:39:24 -0700

    Style fixed

--- scripts/my_script.py
+++ scripts/my_script.py
@@ -1,7 +1,7 @@
 """Script to run some part of my project."""
 
 # This adds the directory above to our Python path
-#   This is so that we can add import our custom python module code into this script
+# This is so that we can add import our custom python module code into this script
 import sys
 sys.path.append('../')
 
@@ -11,10 +11,17 @@ from my_module.functions import send_email, send_timed_email
 
 ###
 ###
-hws = [ ]
+
+sender = input("Which email address do you want the reminders to be sent from?")
+receiver = input("Which email address do you want to receive your reminders?")
+
+hws = [ ] # List of homework
 t = dt.datetime.today()
+
+# Keep reading in user input until user indicates "Done"
 while True:
-    hw = input("What homework do you have? (Enter Done if you don't have any homework to enter)")
+    hw = input("What homework do you have? " + 
+         "(Enter Done if you don't have any homework to enter)")
     if hw == "Done":
         break
 
@@ -30,13 +37,15 @@ while True:
     except TypeError:
         print("Please enter integers as hour, month, day")
     except ValueError:
-        print("Please enter valid values for hour(00-23), month(01-12), day(01-31)")
+        print("Please enter valid values for " + 
+              "hour(00-23), month(01-12), day(01-31)")
     
-    date = input("When do you want to be reminded of doing this homework? (hr mm dd)")
+    date = input("When do you want to be reminded of doing this homework?" + 
+                 " (hr mi mm dd)")
     date_h = int(date[0:2])
-    date_m = int(date[3:5])
-    date_d = int(date[6:8])
-    date_min = int(date[9:11]) # REMOVE LATER
+    date_min = int(date[3:5])
+    date_m = int(date[6:8])
+    date_d = int(date[9:11])
     date_dt = t
     try:
         if date_m < t.month:
@@ -46,7 +55,8 @@ while True:
         print("Please enter integers as hour, month, day")
     except ValueError:
         print("Please enter valid values for hour(00-23), month(01-12), day(01-31)")
+        
     hws.append(tuple((hw, ddl_dt, date_dt)))
 
 password = input("Type your password and press enter: ")
-send_timed_email(password, hws)
+send_timed_email(password, sender, receiver, hws)
