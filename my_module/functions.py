ce506300a2ac454ae97e24d236a5c5e415430d57
Cheery Wang <cheeryone821@gmail.com>
2019-06-11 18:14:44 -0700

    done

--- my_module/functions.py
+++ my_module/functions.py
@@ -1,35 +1,104 @@
 """A collection of function for doing my project."""
 
-import datetime as dt
 import time
 
 # Import smtplib for the actual sending function
-import smtplib, ssl
+import smtplib
+import ssl
 
-def send_email(password, hw, ddl):
+import datetime as dt
+
+
+def send_email(password, sender, receiver, hw):
+    """ Send one homework email reminder. This function is mostly an adaptation
+    from code on this website: https://realpython.com/python-send-email/#getting-started.
+    The only part I contributed is the message customization based on the homework
+    parameter.
+    
+    Parameters
+    ----------
+    password : str
+        The password to sender's email address. 
+    sender : str
+        Email reminder sender's email address.
+    receiver : str
+        Email reminder receiver's email address.
+    hw : tuple
+        A tuple whose elements are homework name (str), reminder date (dt.datetime), 
+        and deadline (dt.datetime)
+    
+    Returns
+    -------
+    None 
+    """
+
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
-    sender_email = "cheeryprojecttesting@gmail.com"  # My testing email address
-    receiver_email = "qiw131@ucsd.edu"  # My UCSD email address
-    #password = password
-    ddl_str = ddl.strftime("%Y-%m-%d %H:%M")
+    sender_email = sender
+    receiver_email = receiver
+    ddl_str = hw[1].strftime("%Y-%m-%d %H:%M")
     message = """\
     Subject: Homework %s Reminder
 
-    You need to finish %s before %s.""" % (hw, hw, ddl_str)
+    You need to finish %s before %s.""" % (hw[0], hw[0], ddl_str)
 
     context = ssl.create_default_context()
+
+    # Email login and sending handler
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message)
 
-def send_email_at(password, hw, ddl, date):
-    # if for some reason this script is still running
-    # after a year, we'll stop after 365 days
-    for i in range(0,365):
-        t = dt.datetime.today()
-        time.sleep((date-t).seconds)
 
-        send_email(password=password, hw=hw, ddl=ddl)
+def remind_date(t):
+    """ A helper function that gets reminder date from hw tuple in send_timed_email(). 
+    
+    Parameters
+    ----------
+    t : tuple
+        hw variable passed in from send_timed_email. 
+    
+    Returns
+    -------
+    t[-1] : dt.datetime
+        The date when the reminder needs to be sent. 
+    """
+
+    return t[-1]
+
+
+def send_timed_email(password, sender, receiver, hws):
+    """ Send reminder emails for every homework. I got ideas for this function
+    from this post: 
+    https://stackoverflow.com/questions/2031111
+    /in-python-how-can-i-put-a-thread-to-sleep-until-a-specific-time
+    Line 94 and 95 are an adaptation of the code from this post. All other parts
+    are original 
+    
+    Parameters
+    ----------
+    password : str
+        The password to sender's email address. 
+    sender : str
+        Email reminder sender's email address.
+    receiver : str
+        Email reminder receiver's email address.
+    hws : list of tuples
+        A list of tuple whose elements are homework name (str), reminder date 
+        (dt.datetime), and deadline (dt.datetime)
+    
+    Returns
+    -------
+    None 
+    """
+
+    hws = sorted(hws, key=remind_date)
+
+    #  Send email reminder for each homework at its specified time
+    for hw in hws:
+        t = dt.datetime.today()
+        time.sleep((hw[-1] - t).seconds)
+
+        send_email(password=password, sender=sender, receiver=receiver, hw=hw)
         print("email sent")
 
--- my_module/test_functions.py
+++ my_module/test_functions.py
@@ -4,15 +4,11 @@ Note: because these are 'empty' functions (return None), here we just test
   that the functions execute, and return None, as expected.
 """
 
-from functions import my_func, my_other_func
+import datetime as dt
+from functions import send_timed_email, remind_date
 
 ##
 ##
 
-def test_my_func():
-
-    assert my_func() == None
-
-def test_my_other_func():
-
-    assert my_other_func() == None
+def test_remind_date():
+    assert remind_date((9, 0)) == 0
